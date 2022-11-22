003004c1167c66ac0b21566e700d15bc7fa5e3a4
IvanSmolentseff <112988035+IvanSmolentseff@users.noreply.github.com>
2022-11-23 01:53:53 +0300

    Add solution of FinalTestTask

---
+++ FinalTestTask/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача: Смотри readme файл */
+
+using static System.Console;
+
+Clear();
+
+Write("Введите массив строк через пробел: ");
+string[] stringArray = ReadLine().Split(' ');
+
+string[] resultStringArray = GetResultStringArray(stringArray);
+PrintArray(resultStringArray);
+
+
+void PrintArray(string[] array)
+{
+    if (array.Length == 0)
+        WriteLine("Результирующий массив не содержит требуемых строк");
+    else
+        Write("Результирующий массив содержит следующие строки: ");
+        foreach (var item in array)
+            Write($"{item} ");
+}
+
+
+string[] GetResultStringArray(string[] array)
+{
+    int maxWordLength = 3;
+    int count = 0;
+    string[] result = new string[array.Length];
+    foreach (var item in array)
+        if (item.Length <= maxWordLength )
+            { 
+            result[count] = item;
+            count++;
+            }
+    Array.Resize(ref result, count); // *Пояснение к строке кода - смотри readme файл
+    return result;
+}
